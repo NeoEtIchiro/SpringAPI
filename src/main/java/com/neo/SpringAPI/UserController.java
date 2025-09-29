e35f07c3208ed10432c71a3743171e26dce152c9
Neo <neoetichiro@gmail.com>
2025-09-29 10:48:59 +0200

    Connexion à la db + première fonctionnalité (post et get users)

---
+++ src/main/java/com/neo/SpringAPI/UserController.java
@@ -0,0 +1,37 @@
+package com.neo.SpringAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/users")
+public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @PostMapping(path="/add")
+  public @ResponseBody String addNewUser (@RequestParam String name
+      , @RequestParam String email, @RequestParam String password, @RequestParam String role) {
+    // @ResponseBody means the returned String is the response, not a view name
+    // @RequestParam means it is a parameter from the GET or POST request
+
+    User n = new User();
+    n.setName(name);
+    n.setEmail(email);
+    n.setPassword(password);
+    n.setRole(role);
+    userRepository.save(n);
+    return "Saved";
+  }
+
+  @GetMapping(path="/all")
+  public @ResponseBody Iterable<User> getAllUsers() {
+    // This returns a JSON or XML with the users
+    return userRepository.findAll();
+  }
+}
